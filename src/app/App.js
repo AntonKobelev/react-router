92769f359c9587c638f5efd6c908db85f1ade5f3
antonkobelev <antonkobelev85@mail.ru>
2023-03-20 17:25:30 +0500

    add Switch

--- src/app/App.js
+++ src/app/App.js
@@ -1,19 +1,23 @@
 import NavBar from "./components/navBar";
-import { Route } from "react-router-dom";
+import { Route, Switch } from "react-router-dom";
 import Home from "./components/home";
 import Dashboard from "./components/dashboard";
 import Login from "./components/login";
 import Posts from "./components/posts";
+import Stats from "./components/stats";
 
 function App() {
   return (
     <div>
       <NavBar />
       <h1>App</h1>
-      <Route exact path="/" component={Home}></Route>
-      <Route path="/dashboard" component={Dashboard}></Route>
-      <Route path="/login" component={Login}></Route>
-      <Route path="/posts" component={Posts}></Route>
+      <Switch>
+        <Route exact path="/" component={Home}></Route>
+        <Route path="/dashboard/stats" component={Stats}></Route>
+        <Route path="/dashboard" component={Dashboard}></Route>
+        <Route path="/login" component={Login}></Route>
+        <Route path="/posts" component={Posts}></Route>
+      </Switch>
     </div>
   );
 }
