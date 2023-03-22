df95a119c9f4ebf4c023037484d28d304c4d9d40
antonkobelev <antonkobelev85@mail.ru>
2023-03-22 14:41:44 +0500

    add optional params

--- src/app/components/post.jsx
+++ src/app/components/post.jsx
@@ -1,17 +1,15 @@
 import React from "react";
 
-const Post = ({ match, posts }) => {
-  // получаем текущий адрес поста из адресной строки (его id)
-  const postId = match.params.postId;
+const Post = ({ id, posts }) => {
   // создадим функцию для проверки того, есть ли такой пост в нашей базе постов posts (получить post по id)
   // передаем аргумент id для его проверки
   const getPostById = (id) => {
     return posts.find((post) => post.id.toString() === id);
   };
   // создаем переменную для искомого поста
-  const post = getPostById(postId);
+  const post = getPostById(id);
 
-  return <h3>{post ? post.label : `Post with id: ${postId} not founded`}</h3>;
+  return <h3>{post ? post.label : `Post with id: ${id} not founded`}</h3>;
 };
 
 export default Post;
--- src/app/components/posts.jsx
+++ src/app/components/posts.jsx
@@ -1,10 +1,23 @@
 import React from "react";
+import Post from "./post";
+import PostsList from "./postsList";
 
-const Posts = () => {
-  return (
-    <div>
-      <h1>Posts</h1>
-    </div>
+const Posts = ({ match }) => {
+  const postId = match.params.postId;
+  const posts = [
+    { id: 1, label: "post1" },
+    { id: 2, label: "post2" },
+    { id: 3, label: "post3" },
+  ];
+  const display = match.params.display;
+
+  return postId ? (
+    <>
+      {display ? <h3>{display}</h3>:""}
+      <Post id={postId} posts={posts} />
+    </>
+  ) : (
+    <PostsList posts={posts} />
   );
 };
 
